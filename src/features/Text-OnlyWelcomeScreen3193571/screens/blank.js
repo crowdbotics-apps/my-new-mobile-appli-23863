7e40ba2a015eb61bd910ce708d02cab8726ec623
Crowdbotics <team@crowdbotics.com>
2021-01-15 17:32:51 +0000

    add component Text-Only Welcome Screen

---
+++ src/features/Text-OnlyWelcomeScreen3193571/screens/blank.js
@@ -0,0 +1,72 @@
+import { connect } from "react-redux"
+import React from "react"
+import {
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  Button,
+  Switch,
+  TextInput,
+  StyleSheet,
+  ScrollView
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
+
+class Blank extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+  static navigationOptions = ({ navigation }) => {
+    return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
+  }
+
+  render = () => (
+    <View style={styles.View_1}>
+      <Text style={styles.Text_3}>Welcome, User</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  View_1: {
+    width: "100%",
+    alignSelf: "center",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  Text_3: {
+    marginTop: 5,
+    marginBottom: 5,
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingTop: 5,
+    paddingBottom: 5,
+    fontSize: 21,
+    color: "#4c3196",
+    fontFamily: "OpenSans-Bold",
+    textAlign: "left",
+    textAlignVertical: "center",
+    textDecorationLine: "none",
+    textTransform: "none",
+    lineHeight: 12,
+    letterSpacing: 0
+  }
+})
+
+const mapStateToProps = state => {
+  return {}
+}
+
+const mapDispatchToProps = () => {
+  return {}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Blank)
