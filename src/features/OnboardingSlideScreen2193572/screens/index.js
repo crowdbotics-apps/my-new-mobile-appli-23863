d8f655a258f6cac76b38973e2d8863d54cb8d15a
John <45923790+jbenderdev@users.noreply.github.com>
2021-01-15 12:43:27 -0500

    Update index.js

--- src/features/OnboardingSlideScreen2193572/screens/index.js
+++ src/features/OnboardingSlideScreen2193572/screens/index.js
@@ -61,7 +61,7 @@ export default class Blank extends React.Component {
   }
 
   _onDone = () => {
-    this.props.navigation.navigate('LoginSignupScreen177769') // Login/Sign up Screen
+    this.props.navigation.navigate('Text-OnlyWelcomeScreen3193571') // Basic Welcome Screen
   }
 
 
